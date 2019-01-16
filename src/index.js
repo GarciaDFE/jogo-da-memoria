c7a04010477a689f6893573f21e966664b5a8c85
GarciaDFE <garcia.dfe@gmail.com>
2019-01-16 18:59:35 -0200

    [022] - Desafios básico, intermediário e avançado

--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
-const $root = document.querySelector("#root")
+const $root = document.querySelector("#root");
 const $container = document.createElement("section")
-const $memoryCard = document.createElement("article")
-const $memoryCardFront = document.createElement("article");
+const numCards = 10 /* seta o número de cartões do jogo */
 
 const $iconCollab = `
     <img class="icon" 
         src="img/icon-collabcode.png"
         alt="imagem do mascote da CollabCode">
 `
+
 const $iconC = `
     <img class="icon" 
         src="img/icon-c.png"
         alt="imagem do livro de C++">
 `
 
-$container.classList.add("wrap-container")
-$root.insertBefore($container, null)
+createContainer(); /* cria o container */
 
-$memoryCard.classList.add("memory-card")
-$container.insertBefore($memoryCard, null);
+for (let i = 0; i < numCards; i++) {
+  if (i == 0) {
+      createCardFront(); /* cria primeiro cartão com face revelada (frente) */
+  } else {
+      createCardBack(); /* cria cartão com face escondida (verso) */ 
+  }
+}
 
-$memoryCardFront.classList.add("memory-card")
-$memoryCardFront.classList.add("-front");
-$container.insertBefore($memoryCardFront, $memoryCard);
 
-$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab)
-$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+/* função para criar container do jogo */
+function createContainer() {
+    $container.classList.add("wrap-container")
+    $root.insertBefore($container, null)
+}
+
+/* função para cria face padrão do verso do cartão */
+function createCardBack() {
+  const $memoryCard = document.createElement("article");
+  $memoryCard.classList.add("memory-card");
+  $container.insertBefore($memoryCard, null);
+  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+}
+
+/* função para criar face da frente do primeiro cartão */ 
+function createCardFront() {
+  const $memoryCardFront = document.createElement("article");
+  $memoryCardFront.classList.add("memory-card");
+  $memoryCardFront.classList.add("-front");
+  $container.insertBefore($memoryCardFront, null);
+  $memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+}
