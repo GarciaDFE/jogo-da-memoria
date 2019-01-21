de58ff925ef1a7834aa39a51d754960cd2d47053
GarciaDFE <garcia.dfe@gmail.com>
2019-01-21 16:22:14 -0200

    [024] - Desafio 1

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,44 +1,28 @@
-function createMemoryCard() {
-    const $memoryCard = `
+const createMemoryCard = () => {
+    return `
         <article class="memory-card">
             <img
                 class="icon" 
                 src="img/icon-collabcode.png"
                 alt="imagem do mascote da CollabCode" 
                 onclick="handleClick()">
-        </article
+            
+        </article>
         `;
-    
-    return $memoryCard;
 }
 
-function createMemoryCardFront() {
-    const $memoryCard = `
+const createMemoryCardFront = () => {
+    return `
         <article class="memory-card -front">
             <img
                 class="icon" 
                 src="img/icon-c.png"
                 alt="imagem do livro de C++"
                 onclick="handleClick()">
-        </article
+        </article>
         `;
-
-    return $memoryCard;
 }
 
 function handleClick() {
     console.log("Clicado em um cartão!!")
 }
-
-
-/*
-const $Cards = document.querySelectorAll(".memory-card")
-
-$Cards.forEach(function($card, key) {
-    $card.addEventListener("click", mostrarFace)
-})
-
-function mostrarFace() {
-    console.log("Clicado em um cartão!!")
-}
-*/
