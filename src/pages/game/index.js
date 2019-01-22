66b2445d1539821d50a012acb66d1d24fdc3cd2d
GarciaDFE <garcia.dfe@gmail.com>
2019-01-22 17:15:03 -0200

    [025] - Desafio 1

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -4,10 +4,14 @@ const numCards = 8;
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
 
-for (let i = 0; i < numCards; i++) {
-  if (i <= 5) {
-      $cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-collabcode.png", "imagem do mascote da CollabCode"));
-  } else {
-      $cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-c.png", "imagem do livro de C++", "-front"))
-  }
-};
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-c.png", "imagem do livro de C++", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-js.png", "imagem do livro de Javascript", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-java.png", "imagem do livro de Java", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-cabeca.png", "imagem do livro de código na cabeça", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-php.png", "imagem do livro de PHP", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-celular.png", "imagem do livro sobre UX", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-bug.png", "imagem do livro sobre bug", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-responsivo.png", "imagem do livro sobre responsivo", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-woman.png", "imagem do livro de mulher codando", "-front"))
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard("img/icon-settings.png", "imagem do livro sobre configurações", "-front"))
+
