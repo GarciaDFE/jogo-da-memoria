58c8fd521ee33108f7740f2975b799f4b3cf1eed
GarciaDFE <garcia.dfe@gmail.com>
2019-01-24 17:33:47 -0200

    Implementado mais refatoração e organização de pastas da Aula 026

--- src/objects/cards-wrapper/index.js
+++ src/objects/cards-wrapper/index.js
@@ -1,5 +1,22 @@
 function createCardsWrapper() {
     const $cardsWrapper = document.createElement("section")
     $cardsWrapper.classList.add("cards-wrapper")
+
+    const $head = document.querySelector("head")
+    const $style = document.createElement("style")
+    $style.textContent = `
+        .cards-wrapper {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            align-content: flex-start;
+            width: 372px;
+            height: 900px;
+            margin: 0 auto;
+            border: 1px #dadada solid;
+        }
+    `
+    $head.insertBefore($style, null)
+    
     return $cardsWrapper
 }
