7b6ee13693932ab5aa41c5aaa732fc0d29b50ba6
GarciaDFE <garcia.dfe@gmail.com>
2019-03-13 17:10:26 -0300

    [039] - Desafio intermediário

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -29,6 +29,7 @@ const flatButton = (function () {
       $head.insertBefore($style, null);
 
    }
+   // content = "teste" e active = false ==> são formas de setar valor default caso o parâmetro não seja carregado na renderização do elemento
    module.render = (content = "teste", active = false ) => {
       module._id++
       module._style(active)
