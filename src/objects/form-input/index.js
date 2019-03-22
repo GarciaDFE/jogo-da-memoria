9728679dedde47e3c4f565aaaea1130a0c91a223
GarciaDFE <garcia.dfe@gmail.com>
2019-03-22 16:39:38 -0300

    Instalação da lib YUP

--- src/objects/form-input/index.js
+++ src/objects/form-input/index.js
@@ -1,5 +1,23 @@
+//import yup from 'yup';
+
 const formInput = (function() {
    const module = {};
+
+   // const schema = yup.object({
+   //    name: yup.string().required(),
+   //    email: yup.string().test('is-jimmy', '${path} is not Jimmy', function (value) {
+   //       return new Promise((res, rej) => {
+   //          console.log('firing async validation')
+   //          setTimeout(() => res(value !== "foo@bar.com"))
+   //       })
+   //    })
+   // });
+
+
+   module.validate = (input) => {
+         this.setState({[field]: e.target.value})
+         schema.isValid(this.state)
+   }
 
    module._style = () => {
       const $head = document.querySelector("head");
@@ -19,11 +37,18 @@ const formInput = (function() {
          $head.insertAdjacentElement("beforeend", $style);
    };
 
-   module.validate = (input) => {
-      const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      const teste = regex.test(input.value)
-      console.log("Resultado do teste:", teste)
-   }
+   // module.validate = (input) => {
+   //    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   //    const teste = regex.test(input.value)
+   //    console.log("Resultado do teste:", teste)
+   // }
+
+   // module.render = input => {
+   //    module._style();
+   //    return `
+   //       <input id="email" class="form-input" type="email" placeholder="${input}" onInput="formInput.validate(this)">
+   //       `;
+   // };
 
    module.render = input => {
       module._style();
@@ -32,8 +57,9 @@ const formInput = (function() {
          `;
    };
 
+
    return {
       render: module.render,
-      validate: module.validate
+      //validate: module.validate
    };
 })();
